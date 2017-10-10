c8154f019206ab1794afaee26b88af4b575662a1
Baptiste <baptiste.rigondaud@ensimag.fr>
2017-10-10 08:06:05 +0200

    moved interrupt functions to interrupt.c/h

---
+++ src/interrupt.h
@@ -0,0 +1,24 @@
+#ifndef __INTERRUPT_H__
+#define __INTERRUPT_H__
+
+#define FIRST_STACK_SIZE 16384
+#define FIRST_VECTOR_HANDLER_ADDR 0x1000
+#define CMD_PIC_PORT 0x21
+
+#include "inttypes.h"
+#include "segment.h"
+#include "time.h"
+
+/* Prototype for traitant_IT_32 in traitants.S */
+extern void traitant_IT_32(void);
+
+/* The kernel entry point */
+void kernel_start(void);
+
+/* (Un)hides an IT to the PIC */
+void hide_IRQ(uint32_t num_IRQ, uint8_t hide);
+
+/* Init the handler for an IT using the IT number and its handler */
+void init_handler_IT(int32_t num_IT, void (*handler)(void));
+
+#endif
--- src/start.c
+++ src/start.c
@@ -1,36 +1,6 @@
 #include "cpu.h"
 #include "stdio.h"
 #include "start.h"
-#include "screen.h"
-
-void init_handler_IT(int32_t num_IT, void (*handler)(void))
-{  
-  int32_t *handler_addr;
-  handler_addr = (int32_t*)(FIRST_VECTOR_HANDLER_ADDR + num_IT*8);
-  
-  int32_t first_word;
-  first_word = ((int32_t)handler) & 0x0000FFFF;
-  first_word |= KERNEL_CS << 16;
-
-  int32_t second_word;
-  second_word = ((int32_t)handler) & 0xFFFF0000;
-  second_word |= (int32_t)0x8E00;
-
-  *handler_addr = first_word;
-  *(handler_addr+1) = second_word;
-}
-
-void hide_IRQ(uint32_t num_IRQ, uint8_t hide)
-{
-  /* Current hidden IRQs */
-  uint8_t IRQ_array = inb(CMD_PIC_PORT);
-  /* Setting the bit num_IRQ to given hide value */
-  hide = hide << num_IRQ;
-  IRQ_array = (IRQ_array & ~(1<<num_IRQ)) | (hide << num_IRQ);
-  /* Sending new hidden IRQs */
-  outb(IRQ_array, CMD_PIC_PORT);
-  
-}
 
 void kernel_start(void)
 {
